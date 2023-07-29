f40e5cc510059e80ffa4cafcfdfbbd8863ce5253
Abdurraouf Etagiuri <roofa.etagiuri1@gmail.com>
2023-07-29 04:26:47 -0600

    added a demo of todo item

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import '../constants/colors.dart';
 
 class TodoItem extends StatelessWidget{
   const TodoItem({Key ? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context){
-    return Container(child: Text("Todo Item"),);
+    return Container(
+      child: ListTile(
+        onTap: () {},
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        tileColor: Colors.white,
+        leading: Icon(
+          Icons.check_box, 
+          color: blue,
+          ),
+        title: Text(
+          "Do Java Homework", 
+          style: TextStyle(
+            fontSize: 16, 
+            color: black, 
+            decoration: TextDecoration.lineThrough,
+          ),
+        ),
+        trailing: Container(
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.symmetric(vertical: 12 ),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: red,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: IconButton(
+            color: Colors.white,
+            iconSize: 18,
+            icon: Icon(Icons.delete),
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
   }
 }
