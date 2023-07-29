dc24b9482d8c65ad217a6df9fb2f74a025dddf70
Abdurraouf Etagiuri <roofa.etagiuri1@gmail.com>
2023-07-29 03:49:23 -0600

    added search bar and ToDo list title

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,30 +8,86 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: backgroundColor,
-      appBar: AppBar(
-        backgroundColor: backgroundColor,
-        elevation: 0,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-          Icon(Icons.menu, 
-          color: black, 
-          size: 30
-          ),
-
-        Container(
-          height: 40,
-          width: 40,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Image.asset('assets/images/avatar.jpg'),
-          ),
-        ),
-      ]),
-    ),
+      appBar: buildAppBar(),
       body: Container(
-        child: Text("This is the home screen"),
+        padding: EdgeInsets.symmetric(
+          horizontal: 15,
+          ),
+        child: Column(
+          children: [
+            searchBox(),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 50,
+                      bottom: 20,
+                    ),
+                    child: Text(
+                      "Todo List:",
+                      style: TextStyle(
+                      fontSize: 30, 
+                      fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+
+          ],
+        ),
       ), 
     );
+  }
+
+
+Widget searchBox(){
+  return Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(0),
+                  prefixIcon: Icon(Icons.search, 
+                  color: black, 
+                  size: 20,
+                  ),
+                  prefixIconConstraints: BoxConstraints(
+                    maxHeight: 20, 
+                    minWidth: 25,
+                    ),
+                    border: InputBorder.none,
+                    hintText: "Search"
+                ),
+              ),
+            );
+}
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: backgroundColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+        Icon(Icons.menu, 
+        color: black, 
+        size: 30
+        ),
+
+      Container(
+        height: 40,
+        width: 40,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.asset('assets/images/avatar.jpg'),
+        ),
+      ),
+    ]),
+  );
   }
 }
